dc79d693ff2617f50af2d177084a8124e49bacdd
Octo <37278690+yuezhangW@users.noreply.github.com>
2022-05-02 11:49:21 +0800

    v1.0
    完成了基本功能的部署

--- popup.js
+++ popup.js
@@ -1,24 +1,25 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+$(document).ready(function () {
+    $.ajax({
+        url:"http://39.96.25.169:8080/dynamic/newest_scheduler",
+        type:'GET',
+        dataType:'json',
+        success: function (result) {
+            console.log(result)
+            $('#dynamicText').text(result.descText)
+            for (var i = 0; i < result.imageUrlList.length; i ++) {
+                console.log(result.imageUrlList[i]);
+                $('#dynamicImages').append(
+                    '<img src="'+result.imageUrlList[i].url+'"class="dynamic-images" id="dynamicImage'+i+'"/>'
+                );
+                $('#dynamicImage'+i).click(function () {
+                   console.log(this.src);
+                   var newTab = window.open(this.src, '_blank');
+                });
+                $('#dynamicPublishTime').text('发布时间：'+result.publishTime);
+                $('#dynamicHref').attr('href', 'https://t.bilibili.com/'+result.id+'?tab=2')
+            }
+        }
+    })
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+})
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
